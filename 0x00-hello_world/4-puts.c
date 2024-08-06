834c6339d21fb5af892180f7115d951900754d43
DoEhab <dohaehab@gmail.com>
2024-08-06 13:41:13 -0700

    fix betty comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,17 @@
+/*
+ * File: 4-puts.c
+ * Auth: Doha Ehab
+ */
+
 #include<stdio.h>
 
+/*
+ *This function prints chars using puts function
+ */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
 	return (0);
 }
