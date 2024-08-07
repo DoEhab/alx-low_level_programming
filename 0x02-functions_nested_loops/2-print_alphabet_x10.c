074649400ffab083d8a4bf2a7ac6e5515fdcad34
DoEhab <dohaehab@gmail.com>
2024-08-07 06:05:43 -0700

    fix betty errors

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: 'print alphabet'
+ * Return: Always 0 (Success)
+ */
+
 void print_alphabet_x10(void)
 {
 	int x = 0;
 	char letter;
 
-	while(x < 10){
-		for(letter = 'a'; letter<='z'; letter++)
+	while (x < 10)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 
 		x++;
