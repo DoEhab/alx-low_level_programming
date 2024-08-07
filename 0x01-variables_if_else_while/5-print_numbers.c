822ddeba3d53813e9fc4c0b52fe01d2625e8bb8d
DoEhab <dohaehab@gmail.com>
2024-08-07 04:35:05 -0700

    fix betty comments

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ * main - Entry point
+ *
+ * Description: 'print 10 digits'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
 	int i;
 
-	for(i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 		printf("%d", i);
 
 	printf("\n");
