fc75e769fd22e64034b462ef87f65147267e3596
DoEhab <dohaehab@gmail.com>
2024-08-06 23:08:06 -0700

    add new line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 
-int main(void){
-printf("with proper grammar, but the outcome is a piece of art,");
+/**
+ * main - Entry point
+ *
+ * Description: 'printf to print a string'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf ("with proper grammar, but the outcome is a piece of art,\n ");
 
 return(0);
 }
