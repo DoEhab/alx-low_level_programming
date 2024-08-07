860a447b396426412923e748d71b011341c5eb37
DoEhab <dohaehab@gmail.com>
2024-08-07 01:41:14 -0700

    fix betty comments

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+  6  * main - Entry point
+  7  *
+  8  * Description: 'print alphabet'
+  9  * Return: Always 0 (Success)
+ 10  */
+
 int main(void)
 {
 	int i;
-
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
-	for(i = 0; i < 26; i++){
+
+	for (i = 0; i < 26; i++)
 		putchar(alphabet[i]);
-	}	
+
 	putchar('\n');
 	return (0);
 }
