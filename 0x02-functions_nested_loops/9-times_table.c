dcacc830974b6c7ae2af49da08636fb98e2b757d
DoEhab <dohaehab@gmail.com>
2024-08-07 07:38:48 -0700

    fix betty issues

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,15 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 
-void times_table(void){
+void times_table(void)
+{
 	int i;
 	int j;
 
-	for(i = 0;i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for(j = 0;j < 10; j++){
-			printf("%d,",i*j);
+		for (j = 0; j < 10; j++){
+			printf("%d ,", i*j);
 		}
-		printf("\n");	
+		printf("\n");
 	} 
 }
