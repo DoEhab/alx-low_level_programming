c757dbdfd2317079fcf2a33514c68a7ee114c476
DoEhab <dohaehab@gmail.com>
2024-08-07 01:51:20 -0700

    fix betty comments

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
-  6  * main - Entry point
-  7  *
-  8  * Description: 'print alphabet'
-  9  * Return: Always 0 (Success)
- 10  */
+ * main - Entry point
+ *
+ * Description: 'print alphabet'
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
