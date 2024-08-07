24a44a85585769fe408d998722ba92f3761b696f
DoEhab <dohaehab@gmail.com>
2024-08-07 04:43:24 -0700

    fix betty comments

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'print last digit'
+ * Return: Always 0 (Success)
+ */
+
 int main (void)
 {
 	int i;
 
-	char alphabt[] = "abcdfghijklmnoprstuvwxyz";
+	char alphabt[] = "abcdfghijklmnoprstuvwxyz\n";
 	for (i = 24; i >= 0; i--)
 		putchar(alphabt[i]);
 
-	putchar('\n');
 	return (0);
 }
