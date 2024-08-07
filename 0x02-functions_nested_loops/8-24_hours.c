fb6987bd9979738af53301cf24eb67cb5a4d136a
DoEhab <dohaehab@gmail.com>
2024-08-07 07:30:42 -0700

    fix betty issues

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+  * jack_bauer - Entry point
+  *
+  * Description: 'print the time every hour'
+  * Return: no return value
+  */
+
 void jack_bauer(void)
 {
 	int hours_counter = 0;
 	int minutes_counter = 0;
 
-	while(hours_counter < 24)
+	while (hours_counter < 24)
 	{
-		while(minutes_counter < 60)
+		while (minutes_counter < 60)
 		{
-			if(hours_counter < 10)
-				printf("0%d:",hours_counter);
+			if (hours_counter < 10)
+				printf("0%d:", hours_counter);
 			else 
-				printf("%d:",hours_counter);
-			if(minutes_counter<10)
+				printf("%d:", hours_counter);
+			if(minutes_counter < 10)
 				printf("0%d", minutes_counter);
 			else
 				printf("%d", minutes_counter);
@@ -25,7 +32,6 @@ void jack_bauer(void)
 		hours_counter++;
 		minutes_counter = 0;
 	}
-	
 
 }
 
