e7ffcd77f713e937a03c8e3f256e8d91394d69f7
DoEhab <dohaehab@gmail.com>
2024-08-07 06:42:17 -0700

    fix betty issues

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,16 @@
 #include "main.h"
 #include <ctype.h>
 
+/**
+  * _islower - Entry point
+  *
+  * Description: 'check lower case'
+  * Return: Always 0 (Success)
+  */
+
 int _islower(int c)
 {
-	if(islower(c))
+	if (islower(c))
 		return 1;
 	else 
 		return 0;
