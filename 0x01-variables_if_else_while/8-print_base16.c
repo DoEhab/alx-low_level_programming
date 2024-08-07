ab066fb7fc782f19ae468238f9e2f2e9e3b3386e
DoEhab <dohaehab@gmail.com>
2024-08-07 04:54:15 -0700

    fix betty comments

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,10 +13,10 @@ int main(void)
 	char letter;
 
 	for (i = 0; i < 10; i++)
-		putchar('0'+i);
+		putchar('0' + i);
 
 	for (letter = 'a'; letter <= 'f'; letter++)
-       		putchar(letter);
+		putchar(letter);
 
 
 	putchar('\n');
