d0f2e7fa31047b9b9cde600a7599af2eb02b94bf
DoEhab <dohaehab@gmail.com>
2024-08-07 07:41:46 -0700

    fix betty issues

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,8 +8,9 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++){
-			printf("%d ,", i*j);
+		for (j = 0; j < 10; j++)
+		{
+			printf(" %d,", i * j);
 		}
 		printf("\n");
 	} 
