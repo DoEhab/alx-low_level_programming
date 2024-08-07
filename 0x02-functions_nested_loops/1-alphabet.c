0b48cf078d421f374b889066fde1a4fcf1cf7380
DoEhab <dohaehab@gmail.com>
2024-08-07 05:54:47 -0700

    fix betty errors

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-  * main - Entry point
-  *
-  * Description: 'print alphabet'
-  * Return: Always 0 (Success)
-  */
+ * main - Entry point
+ *
+ * Description: 'print alphabet'
+ * Return: Always 0 (Success)
+ */
 
 void print_alphabet(void)
 {
