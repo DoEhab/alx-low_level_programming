9e06a51ed961d60b8def9d7a67932bf47cd6bd23
DoEhab <dohaehab@gmail.com>
2024-08-07 03:11:10 -0700

    fix betty comments

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'print alphabet in lower and upper case'
+ * Return: Always 0 (Success)
+ */
+
 int main(void){
 
 	int i;
-
 	char alphabets[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	for(i = 0; i < 56; i++){
+	
+	for (i = 0; i < 52; i++)
 		putchar(alphabets[i]);
-	}
+	
 	putchar('\n');
 	return (0);
 }
