750cc107efe633f802e2a16c0e0aea83bda42082
DoEhab <dohaehab@gmail.com>
2024-08-07 07:47:06 -0700

    fix betty issues

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,14 @@
 #include "main.h"
 
+/**
+  * add - add 2 number
+  *
+  * @x: first number
+  * @y: second number
+  * Return: the sum of 2 numbers
+  */
+
 int add(int x, int y)
 {
-	return(x+y);
+	return (x + y);
 }
