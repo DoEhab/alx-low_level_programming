a9d734bfaf7c379ccd8d77b96c2fc8e34d28a63e
DoEhab <dohaehab@gmail.com>
2024-08-07 07:18:15 -0700

    fix betty issues

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,8 +1,14 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+  * _abs - check if the num is absolute
+  *
+  * @num: the number to be checked
+  * Return: return 1 if true and 0 if false
+  */
 
-int _abs(int num){
-
+int _abs(int num)
+{
 	return (abs(num));
 }
