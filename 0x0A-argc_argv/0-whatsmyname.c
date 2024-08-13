84ebe0080bec716f70107761d64aa0bda0e4a6a9
DoEhab <dohaehab@gmail.com>
2024-08-13 06:14:25 -0700

    fix betty commits

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+  * main - Entry point
+  *
+  * @argc: arguments count
+  * @argv: arguments value
+  * Return: Always 0 (Success)
+  */
+
 int main(int argc, char *argv[])
 {
 	printf("%s \n", argv[0]);
