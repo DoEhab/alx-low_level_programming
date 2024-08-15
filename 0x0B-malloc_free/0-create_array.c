8da4d1ef49fd8a5633fd1573e2a1c8a9da7a4216
DoEhab <dohaehab@gmail.com>
2024-08-15 13:49:38 -0700

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * create_array: creates array of char
+  *
+  * @size: size of array
+  * @c: char to be initialize the array
+  * Return: pointer to char array
+  */
+
+char *create_array(unsigned int size, char c)
+{
+	char *result = malloc(size * (sizeof(c)));
+	unsigned int i;
+
+	if (result == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		result[i] = c;
+
+	return (result);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,7 @@
+#ifndef ALX_H
+#define ALX_H
+
+void _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+#endif 
