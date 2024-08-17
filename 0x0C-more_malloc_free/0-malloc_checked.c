91bc99ccc18b0fc8582fb8da7c0c6dca86782b11
DoEhab <dohaehab@gmail.com>
2024-08-17 10:51:18 -0700

    add task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * malloc_checked - returns pointer to allocated memory
+  *
+  * @b: size to be allocated
+  * Return: void pointer
+  */
+
+void *malloc_checked(unsigned int b)
+{
+	void *result = malloc(b);
+
+	if (result == NULL)
+		exit(98);
+
+	return (result);
+}
