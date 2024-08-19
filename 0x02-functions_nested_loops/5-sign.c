374189c6f4bd236d013f0a786aa42616f2079ef2
DoEhab <dohaehab@gmail.com>
2024-08-19 15:04:31 -0700

    fix _putchar

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,18 +1,26 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+  * print_sign - prints the sign of a number
+  *
+  * Description: 'print signs'
+  * @n: the number to be used
+  * Return: -1 for negative numbers, 1 for positive and 0 for zer
+  */
+
 int print_sign(int n)
 {
 	if (n < 0)
 	{
 		_putchar('-');
-		return -1;
-	}	
+		return (-1);
+	}
 	if (n > 0)
-        {
-                _putchar('+');
-                return 1;
-        }else 
-		_putchar('0');
-	return 0;
+	{
+		_putchar('+');
+		return (1);
+	}
+	_putchar('0');
+	return (0);
 }
