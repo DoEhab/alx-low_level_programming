4856895bcef81463e93e627ca4e6156c9309b350
DoEhab <dohaehab@gmail.com>
2024-08-19 15:24:25 -0700

    0-upper task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,17 @@
 #include "main.h"
 #include <ctype.h>
 
+/**
+  * isupper - check if the letter is upper case
+  *
+  * @c: the letter to be checked
+  * Return: return 1 if true and zero if false
+  */
+
 int _isupper(int c)
 {
-	if(isupper(c))
-		return 1;
-	else 
-		return 0;
+	if (isupper(c))
+		return (1);
+	else
+		return (0);
 }
