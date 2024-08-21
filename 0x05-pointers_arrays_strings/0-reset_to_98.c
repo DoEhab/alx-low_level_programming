58428f99c639f84d941cc3908e3c2e890ebce020
DoEhab <dohaehab@gmail.com>
2024-08-21 13:08:37 -0700

    add readme file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+  * reset_to_98 - Assign the the pointer value
+  *
+  * @n: pointer to the variable
+  * Return: No return value
+  */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
