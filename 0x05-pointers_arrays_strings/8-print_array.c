c175c691eeb1fcd3146dd0e213ba2efa2f0c276b
DoEhab <dohaehab@gmail.com>
2024-08-23 13:04:21 -0700

    fix odd number issue

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_array - prints an array based on a given lenght
+  *
+  * @a: pointer to the array
+  * @n: lenght to be printed
+  * Return: void
+  */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+		printf("%d,", a[i]);
+	printf("%d", a[i]);
+	printf("\n");
+}
