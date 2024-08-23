677d1ddb82bee4b3534c4cb0390a1882a823cb85
DoEhab <dohaehab@gmail.com>
2024-08-23 11:59:45 -0700

    remove extra line

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,10 +13,9 @@ void puts2(char *str)
 	int len = strlen(str);
 	int i = 0;
 
-	while (i < (len - 1))
+	while (i < len)
 	{
-		 _putchar(str[i]);
+		_putchar(str[i]);
 		i = i + 2;
 	}
-	_putchar('\n');
 }
