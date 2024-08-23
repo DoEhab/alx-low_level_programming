35b839110f9a8f9a08b198157db9505f245e89e0
DoEhab <dohaehab@gmail.com>
2024-08-23 10:44:26 -0700

    fix betty errors

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,13 +16,13 @@ void print_to_98(int n)
 	{
 		for (i = n; i > 98; i--)
 			printf("%d, ", i);
-	printf("%d \n", i);
+	printf("%d\n", i);
 	}
 	else
 	{
 		for (i = n; i < 98; i++)
 			printf("%d, ", i);
 
-	printf("%d \n", i);
+	printf("%d\n", i);
 	}
 }
