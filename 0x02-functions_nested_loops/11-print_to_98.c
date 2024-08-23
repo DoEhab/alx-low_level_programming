2d6f339a0bb12d5494ca3578e3074aa5337215db
DoEhab <dohaehab@gmail.com>
2024-08-23 10:42:43 -0700

    fix betty errors

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,18 +1,28 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+  * print_to_98 - print numbers starting n to 98
+  *
+  * @n: the starting number
+  * Return: No return value
+  */
+
 void print_to_98(int n)
 {
 	int i;
 
-	if(n > 98)
+	if (n > 98)
 	{
-		for(i = n; i >= 98; i--)
-                printf("%d, ", i);
-
+		for (i = n; i > 98; i--)
+			printf("%d, ", i);
+	printf("%d \n", i);
 	}
-	for(i = n; i <= 98; i++)
-		printf("%d, ", i);
+	else
+	{
+		for (i = n; i < 98; i++)
+			printf("%d, ", i);
 
-	printf("\n");
+	printf("%d \n", i);
+	}
 }
