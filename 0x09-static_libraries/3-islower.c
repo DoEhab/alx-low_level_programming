a0d22646295b42707a45fb1bb887c90f8d8b2253
DoEhab <dohaehab@gmail.com>
2024-08-25 04:32:23 -0700

    0x09 task 0

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+  * _islower - return if lower case digit
+  *
+  * @c: char to be checked
+  * Return: 1 if lower case and zero if not
+  */
+
+int _islower(int c)
+{
+	if (islower(c))
+		return (1);
+	else
+		return (0);
+}
