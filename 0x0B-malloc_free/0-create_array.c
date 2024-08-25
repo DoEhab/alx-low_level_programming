05e6a92c11fb6acf185627e2da8b46ab77a47589
DoEhab <dohaehab@gmail.com>
2024-08-25 08:01:26 -0700

    fix null check

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,7 +4,6 @@
 /**
   * create_array: creates array of char
   *
-  * Description: 'create an array of char and initializes it with char'
   * @size: size of array
   * @c: char to be initialize the array
   * Return: pointer to char array
@@ -16,6 +15,9 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
+		return (NULL);
+
+	if (result == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
