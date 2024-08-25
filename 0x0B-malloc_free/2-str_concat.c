08e1efd58fc751ab3c9d45921872f8fde9f3c6c6
DoEhab <dohaehab@gmail.com>
2024-08-25 10:48:58 -0700

    fix betty

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,14 +16,11 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	char *result;
 
-	if (s1 && s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
 
-	if (s1 == NULL && s2 != NULL)
-		return (s2);
-
-	if (s1 != NULL && s2 == NULL)
-		return (s1);
+	if (s2 == NULL)
+		s2 = "";
 
 	for (i = 0; s1[i]; i++)
 		len_1++;
