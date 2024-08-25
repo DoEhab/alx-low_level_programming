4c1732b1436bc31e119174b3c6c9fdda82c6a1bf
DoEhab <dohaehab@gmail.com>
2024-08-25 08:57:06 -0700

    fix betty warnings

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,8 +26,14 @@ int **alloc_grid(int width, int height)
 	}
 
 	for (i = 0; i < height; i++)
+	{
 		arr[i] = malloc(sizeof(int) * width);
-
+		if (arr[i] == NULL)
+		{
+			free(arr[i]);
+			return (NULL);
+		}
+	}
 	if (arr == NULL)
 	{
 		free(arr);
