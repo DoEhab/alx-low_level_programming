6548f3eb67c5a6fc2c0831dca8079d5c8c7b8559
DoEhab <dohaehab@gmail.com>
2024-08-25 08:30:06 -0700

    add description in comments

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,6 +1,14 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+  * free_grid - frees memory based on received params
+  *
+  * @grid: 2d array
+  * @height: array size
+  * Return: Always 0 (Success)
+  */
+
 void free_grid(int **grid, int height)
 {
 	int i;
