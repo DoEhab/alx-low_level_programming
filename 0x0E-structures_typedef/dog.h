395177d5926bc9223a575b578b747a9d67368c42
DoEhab <dohaehab@gmail.com>
2024-08-26 10:58:16 -0700

    add dog header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines dog specs
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog age
+ *
+ * Description: struct to define dog name, age, owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
