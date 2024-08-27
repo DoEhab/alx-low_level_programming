17f58f8e483e88c782d1a4cd888b4fecedb8c6cd
DoEhab <dohaehab@gmail.com>
2024-08-27 04:57:46 -0700

    fix betty comment

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,14 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+/**
+  * main - print the sum of args
+  *
+  * @argc: arguments count
+  * @argv: areguments value
+  * Return: Always 0 (Success)
+  */
+
 int main(int argc, char *argv[])
 {
 	if (argc < 2)
@@ -16,19 +24,19 @@ int main(int argc, char *argv[])
 		for (i = 0; i < argc; i++)
 		{
 			int val = atoi(argv[i]);
+
 			if (isdigit(val))
 			{
-				printf("Error \n");
+				printf("Error\n");
 				return (1);
- 
 			}
-			else 
+			else
 			{
 				sum += val;
 			}
 		}
 
-		printf("%d \n", sum);
+		printf("%d\n", sum);
 	}
 	return (0);
 }
