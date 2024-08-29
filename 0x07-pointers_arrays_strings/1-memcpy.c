2a07a538ad9c16ad4770de2e46275a93c2b476c6
DoEhab <dohaehab@gmail.com>
2024-08-29 13:14:46 -0700

    fix betty errors

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+  * _memcpy - copy data from src arr to dest arr
+  *
+  * @dest: result
+  * @src: source array
+  * @n: size
+  * Return: destination array
+  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
@@ -7,5 +16,5 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	for (i = 0; i < n; i++)
 		dest[i] = src[i];
 
-	return dest;
+	return (dest);
 }
