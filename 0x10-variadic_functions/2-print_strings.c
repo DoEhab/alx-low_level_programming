ea177efd930a50022847a7f26a0ca9b5f2f316db
DoEHab <dohaehab@gmail.com>
2024-09-02 14:34:04 -0700

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		result = va_arg(args, char*);
 		printf("%s", result);
 		if (separator == NULL)
-			printf("nil")
+			printf("nil");
 		else if (i < n - 1)
 			printf("%s", separator);
 	}
