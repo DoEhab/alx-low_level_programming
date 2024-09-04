db3e70ca3bdc14bcc3f0483e26f86694f60bf600
DoEHab <dohaehab@gmail.com>
2024-09-04 03:27:57 -0700

    add nill

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (result != NULL)
 			printf("%s", result);
 		else
-			printf("nil");
+			printf("(nil)");
 		if (separator == NULL)
 			continue;
 		if (i < n - 1)
