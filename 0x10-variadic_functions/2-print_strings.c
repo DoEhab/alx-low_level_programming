d01a97b3da93d82138b7cb27ffabd9b8f920cd3b
DoEHab <dohaehab@gmail.com>
2024-09-04 03:16:17 -0700

    add null check

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,10 +20,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (; i < n; i++)
 	{
 		result = va_arg(args, char*);
-		printf("%s", result);
+		if (result != NULL)
+			printf("%s", result);
 		if (separator == NULL)
-			printf("nil");
-		else if (i < n - 1)
+			continue;
+		if (i < n - 1)
 			printf("%s", separator);
 	}
 	printf("\n");
