3af45b47c4561f1524971f34111c78341e3c43a1
DoEHab <dohaehab@gmail.com>
2024-09-06 14:07:37 -0700

    task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * get_bit - specifies whether the value is 0 or 1
+  *
+  * @n: the number in decimal
+  * @index: the index to get the value for
+  * Return: return the binary value of index
+  */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+	return (1);
+}
