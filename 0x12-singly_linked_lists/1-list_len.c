f21d8a8170721cec002a518befbc8c770ace695d
dohaehab <dohaehab@gmail.com>
2024-09-15 23:51:35 +0300

    betty issues

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,12 +10,12 @@
 
 size_t list_len(const list_t *h)
 {
-    size_t list_count = 0;
+	size_t list_count = 0;
 
-    while (h)
-    {
-        list_count += 1;
-        h = h->next;
-    }
-    return (list_count);
+	while (h)
+	{
+		list_count += 1;
+		h = h->next;
+	}
+	return (list_count);
 }
