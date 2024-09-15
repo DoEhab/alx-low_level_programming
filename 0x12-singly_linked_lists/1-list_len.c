2bcfd4573d095b2273b7a86d00c8c7ffdf7da892
dohaehab <dohaehab@gmail.com>
2024-09-15 23:45:47 +0300

    linked list task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,14 +1,21 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * list_len - returns linked list size
+ *
+ * @h: pointer to the first node
+ * Return: number of nodes
+ */
+
 size_t list_len(const list_t *h)
 {
     size_t list_count = 0;
 
-    while(h)
+    while (h)
     {
         list_count += 1;
         h = h->next;
     }
-    return list_count;
+    return (list_count);
 }
