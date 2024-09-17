41ac8b57f3f27609368e1c4c8471bca6d4a6f25f
dohaehab <dohaehab@gmail.com>
2024-09-17 23:53:17 +0300

    betty issues

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,19 +3,25 @@
 #include <string.h>
 #include <stdlib.h>
 
+/**
+ * add_node - adds new node to the linked list
+ *
+ * @head: pointer to the new node
+ * @str: the string value
+ */
+
 list_t *add_node(list_t **head, const char *str)
 {
-        list_t *new_node;
+	list_t *new_node;
 
-        new_node = malloc(sizeof(list_t));
-        if (new_node == NULL)
-                return NULL;
-        
-        new_node->str = strdup(str);
-        new_node->len = strlen(str);
-        new_node->next = *head;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-        *head = new_node;
-        return new_node;
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = *head;
 
+	*head = new_node;
+	return (new_node);
 }
