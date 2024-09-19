7d831b80cbcbf469f8160d321f3b1a1eb0c06dce
dohaehab <dohaehab@gmail.com>
2024-09-20 00:45:07 +0300

    task 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -19,19 +19,19 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	new_node->next = NULL;
 	new_node->n = n;
+
 	if (*head == NULL)
 	{
-		*head = new_node;
 		new_node->prev = NULL;
+		*head = new_node;
 		return (new_node);
 	}
 
 	temp = *head;
 	while (temp->next != NULL)
 		temp = temp->next;
-
 	temp->next = new_node;
 	new_node->prev = temp;
 
-	return (new_node);    
+	return (new_node);
 }
