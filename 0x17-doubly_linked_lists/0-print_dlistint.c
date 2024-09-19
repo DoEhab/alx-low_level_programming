e969d2f5433d6ac809e78a791a09e7b59e32a181
dohaehab <dohaehab@gmail.com>
2024-09-19 23:03:45 +0300

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,7 +9,7 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	int len = 0;
+	size_t len = 0;
 
 	while(h)
 	{
@@ -17,6 +17,6 @@ size_t print_dlistint(const dlistint_t *h)
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-	return len;
+	return (len);
 }
 
