85ec923b2fd1a18ba1b95037af0c9548ed8198e5
dohaehab <dohaehab@gmail.com>
2024-09-20 02:55:31 +0300

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,6 +22,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	new_node->n = n;
 
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		new_node->prev = NULL;
+
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return new_node;
+	}
+
 	temp = *h;
 	while (temp != NULL)
 	{
