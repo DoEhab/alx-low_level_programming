726aaa745d04dbc2783eb54289c45a2c976ae982
dohaehab <dohaehab@gmail.com>
2024-09-20 01:00:31 +0300

    task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free linked list nodes
+ *
+ * @head: pointer to the starting node
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
