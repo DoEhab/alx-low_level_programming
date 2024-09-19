861da76e4a1ea3a4e9aaab6fb3b79b8d8f753d2f
dohaehab <dohaehab@gmail.com>
2024-09-19 23:35:42 +0300

    task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,20 +1,19 @@
 #include "lists.h"
 
 /**
-  * print_dlistint - return the length of linked list
-  *
-  * @h: pointer to the starting node
-  * Return: length of linked list
-  */
+ * dlistint_len - return the length of linked list
+ *
+ * @h: pointer to the starting node
+ * Return: length of linked list
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t len = 0;
- 
-	while (h)
-	{
-		if (h->next != NULL)
-			len++;
-		h = h->next;
-	}
-	return len;
+    size_t len = 0;
+
+    while (h)
+    {
+        len++;
+        h = h->next;
+    }
+    return len;
 }
