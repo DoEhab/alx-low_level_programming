fd4e98c51a03c5ed1f3dedea25a8a69b85d90cbf
doehab <dohaehab@gmail.com>
2024-10-11 13:46:31 -0700

    t4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -15,11 +15,14 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
+
 	while (node != NULL)
 	{
 		if (strcmp(node->key, key) == 0)
 			return (node->value);
 		node = node->next;
 	}
+
 	return (NULL);
+
 }
