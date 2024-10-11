c9ca58ce70aec5538632985763a8c05e140bb83c
dohaehab <dohaehab@gmail.com>
2024-10-11 13:13:12 +0300

    hash table T0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/*
+* hash_table_create: creates a new hash table
+*
+* @size: the array size
+* Return: retuns nre hash table and NULL if failed
+*/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i = size;
+
+	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+        	return (NULL);
+
+	hash_table->size = size;
+	hash_table->array = malloc(sizeof(hash_node_t) * size);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
+
+	return hash_table;
+}
