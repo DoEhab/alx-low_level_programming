15f6aa7964f42da805b87463b0b96ab13db247b7
doehab <dohaehab@gmail.com>
2024-10-11 03:32:28 -0700

    ht T0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,11 +17,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table->size = size;
 	hash_table->array = malloc(sizeof(hash_node_t) * size);
-	if (array == NULL)
+	if (hash_table->array == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		array[i] = NULL;
+		hash_table->array[i] = NULL;
 
 	return hash_table;
 }
