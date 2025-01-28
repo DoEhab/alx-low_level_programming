fbc180d7ddc1e0055ac384f158ff7f41cf5079c6
dohaehab <dohaehab@gmail.com>
2025-01-28 23:06:21 +0200

    task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include<stddef.h>
+
+/**
+  * linear_searc - search in the array
+  *
+  * @array: the array to be search
+  * @size: array size
+  * @value: the target value
+  * Return: value if found and -1 otherwise
+  */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+	if ( array == NULL )
+		return (-1);
+
+	for (i = 0; i < size; i++ ){
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+
+}
